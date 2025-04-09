fbe5034ae4f01839539ca632305f7daa2d6f110d
Zachary Knowlan <zachary.knowlan@gmail.com>
2025-04-09 16:07:12 -0400

    Mesh constructor (reader) written.

--- src/Mesh.h
+++ src/Mesh.h
@@ -6,6 +6,7 @@
 #define ASSIGNMENT2_MESH_H
 
 #include <string>
+#include <Kokkos_Core.hpp>
 
 //********************************** Helper Functions
 //****************************//
@@ -13,22 +14,30 @@ void check_file_existence(const std::string filename);
 
 //********************************** Mesh Class
 //**********************************//
-enum class MeshType {
-  TRIANGLE = 0,
-  QUAD = 1,
+enum MeshType 
+{
+  TRIANGLE = 3,
+  QUAD = 4,
 };
 
-class Mesh {
- public:
-  Mesh(const std::string filename);
+class Mesh 
+{
+    public:
+    Mesh(const std::string filename);
 
- private:
-  size_t numVertices;
-  size_t numElements;
+    [[nodiscard]]
+    inline double GetCoordinate(int element, int node, int dim) {return data_(element,node,dim+1);}
 
-  MeshType meshType;
+    [[nodiscard]]
+    inline int GetGlobalDof(int element, int node) {return data_(element,node,0);}
 
-  // data arrays depending on cuda flag
+    private:
+    size_t numVertices_ = 0;
+    size_t numElements_ = 0;
+    MeshType meshType_;
+    Kokkos::View<double***, Kokkos::DefaultHostExecutionSpace> data_; // CHANGE FROM HOST SPACE LATER
+
+    // data arrays depending on cuda flag
 };
 
 #endif  // ASSIGNMENT2_MESH_H
